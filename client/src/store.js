63f231009b891fd4aa009950e78a0f4290757385
Andre Suchitra <89.andre@gmail.com>
2019-05-16 19:07:59 +0700

    connect to firestore getRoom

--- client/src/store.js
+++ client/src/store.js
@@ -1,18 +1,30 @@
+/* eslint-disable no-param-reassign */
 import Vue from 'vue';
 import Vuex from 'vuex';
+import db from './fb';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    gameStatus : ''
+    gameStatus: '',
   },
   mutations: {
-    setGameStatus(state, payload){
+    setGameStatus(state, payload) {
       state.gameStatus = payload;
-    }
+    },
+    setRoom(state, payload) {
+      state.room = payload;
+    },
   },
   actions: {
-
+    getRoom(context) {
+      db.collection('room').onSnapshot((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          context.commit('setRoom', doc.data());
+          console.log('room got...', context.state.room);
+        });
+      });
+    },
   },
 });
