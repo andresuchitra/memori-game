60b05f1f597d0241d2d8e684d2011d19f0be13e9
martinsuhendra <suhendra.martin90@gmail.com>
2019-05-17 09:58:19 +0700

    adding rooms

--- client/src/router.js
+++ client/src/router.js
@@ -15,5 +15,13 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/PlayBoard.vue'),
     },
+    {
+      path: '/rooms',
+      name: 'room',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/Room.vue'),
+    },
   ],
 });
