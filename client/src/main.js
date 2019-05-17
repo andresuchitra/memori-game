216b44264e2aa33005c736e038ece1009bd1d748
rudyfebrianthomas <rudyfebrian6@gmail.com>
2019-05-17 12:04:06 +0700

    chats

--- client/src/main.js
+++ client/src/main.js
@@ -2,6 +2,16 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+firebase.initializeApp({
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_PROJECT_ID
+});
+
+// var db = firebase.firestore();
 
 Vue.config.productionTip = false;
 
